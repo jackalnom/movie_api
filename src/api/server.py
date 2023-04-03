6e8a3ccf038676ea7526e4ac174c1e9934e97ce2
petersonq3181 <54282732+petersonq3181@users.noreply.github.com>
2023-04-03 14:22:16 -0700

    update server.py

--- src/api/server.py
+++ src/api/server.py
@@ -32,8 +32,8 @@ app = FastAPI(
     description=description,
     version="0.0.1",
     contact={
-        "name": "Lucas Pierce",
-        "email": "lupierce@calpoly.edu",
+        "name": "Quinn Peterson",
+        "email": "qpeterso@calpoly.edu",
     },
     openapi_tags=tags_metadata,
 )
